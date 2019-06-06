0946340e6ed4553f4781f134328aa922fd3808d5
Allan Soares Vasconcelos <asv@ug1c08.windows.cin.ufpe.br>
2019-06-06 12:50:49 -0300

    Implementando Chat no Client

--- Client.c
+++ Client.c
@@ -11,6 +11,15 @@
 #define ALTURA_TELA 780
 #define FPS 60.0
 
+//AtributosChat
+
+#define MSG_MAX_SIZE 350
+#define BUFFER_SIZE (MSG_MAX_SIZE + 100)
+#define LOGIN_MAX_SIZE 13
+#define HIST_MAX_SIZE 200
+#define MAX_LOG_SIZE 17
+#define IP_MAX_SIZE 100
+
 void ChecaPonteiro(void * ptr, char erro[50]){
 
 	if(ptr == NULL){
@@ -26,6 +35,7 @@ void startTimer();
 double getTimer();
 void FPSLimit();
 double startingTime;
+void readInput(ALLEGRO_EVENT event, char str[], int limit);
 
 int main(){
 	
@@ -36,6 +46,10 @@ int main(){
 	ALLEGRO_FONT *fonteHTP = NULL, *fonteHTPTitulo = NULL;
 	ALLEGRO_TIMER *timer = NULL;
 	int apertouBotaoPlay = 0, inMenu = 1, inGame = 0, apertouBotaoExit = 0, apertouBotaoHowtoPlay = 0, delay = 0;
+
+	//Variaveis do chat! 
+	ALLEGRO_EVENT_QUEUE *filaEventosChat = NULL;
+	int inChat = 0;
 
 	ALLEGRO_BITMAP *charSprite = NULL;		//Variaveis do personagem!
 	float larguraSprite = 42, alturaSprite = 52.5 ;
@@ -93,9 +107,11 @@ int main(){
 
 	filaEventosMouse = al_create_event_queue(); //Cria as filas de eventos!
 	filaEventosTimer = al_create_event_queue();
+	filaEventosChat = al_create_event_queue();
 
 	ChecaPonteiro(filaEventosMouse, "Erro na fila de eventos Mouse"); 	//Checa erro!
-	ChecaPonteiro(filaEventosTimer, "Erro em filaEventosTimer"); 	//Checa erro!
+	ChecaPonteiro(filaEventosTimer, "Erro em filaEventosTimer"); 	//Checa erro! 
+	ChecaPonteiro(filaEventosChat, "Erro em filaEventosChat"); 	//Checa erro!
 
 	al_register_event_source(filaEventosMouse, al_get_mouse_event_source());	//Fontes dos eventos!
 	al_register_event_source(filaEventosMouse, al_get_keyboard_event_source());
@@ -150,8 +166,121 @@ int main(){
 
 				if(apertouBotaoPlay == 1){			//Logica do nosso jogo ficara aqui!
 
+					//Esse sera o IP do Server!
+					char ServerIP[30]={"127.0.0.1"};
+					int qtdCharIP = 9;
+					//para guardar o log do chat
+					char chatLog[MAX_LOG_SIZE][MSG_MAX_SIZE] = {{0}};
+					//mensagem pra ser mandada no lobby
+					char lobbyMessage[110]={0}; 
+					//para guardar o login
+					char loginMsg[BUFFER_SIZE]={0};
+					int connectScreen = 1;
+					int loginScreen = 1;
+					int lobby = 1;
+
+					
+					connectScreen = 1;
+					inChat = 1;
+					filaEventosChat = al_create_event_queue();	//Reiniciando fila de Eventos!
+					al_register_event_source(filaEventosChat, al_get_mouse_event_source());	//Fontes dos eventos!
+					al_register_event_source(filaEventosChat, al_get_keyboard_event_source());
+
+					while(inChat){	//Iniciando codigos do nosso chat!
+
+						while(connectScreen){ //Tela para digitar o IP
+
+							startTimer();
+
+							while(!al_is_event_queue_empty(filaEventosChat)){
+
+								ALLEGRO_EVENT connectEvent;
+								al_wait_for_event(filaEventosChat, &connectEvent);
+
+								readInput(connectEvent, ServerIP, IP_MAX_SIZE);
+
+								if (connectEvent.type == ALLEGRO_EVENT_KEY_DOWN){
+
+									switch(connectEvent.keyboard.keycode){
+
+										case ALLEGRO_KEY_ENTER:
+											connectScreen = 0;
+											break;
+									}
+										
+								}
+
+							}
+							
+							al_draw_bitmap(BackgroundMenu,0,0,0);	//Printar as informações na tela!
+
+    						al_draw_text(fonteHTPTitulo, al_map_rgb(255, 255, 255), LARGURA_TELA / 2, 30, ALLEGRO_ALIGN_CENTRE, "Digite o IP do server:");
+
+    						if (strlen(ServerIP) > 0){
+
+        						al_draw_text(fonteHTP, al_map_rgb(255, 255, 255), LARGURA_TELA / 2, (ALTURA_TELA - al_get_font_ascent(fonteHTP)) / 2, ALLEGRO_ALIGN_CENTRE, ServerIP);
+    						}else{
+
+        						al_draw_text(fonteHTP, al_map_rgb(255, 255, 255), LARGURA_TELA / 2, (ALTURA_TELA - al_get_font_ascent(fonteHTP)) / 2, ALLEGRO_ALIGN_CENTRE, "0.0.0.0");
+    						}	
+
+							al_flip_display();
+							FPSLimit();
+						}
+
+						while(loginScreen){ //Tela para printar o login!
+
+							startTimer();
+
+							while(!al_is_event_queue_empty(filaEventosChat)){
+
+								ALLEGRO_EVENT loginEvent;
+								al_wait_for_event(filaEventosChat, &loginEvent);
+
+								readInput(loginEvent, loginMsg, LOGIN_MAX_SIZE);
+
+								if (loginEvent.type == ALLEGRO_EVENT_KEY_DOWN){
+
+									switch(loginEvent.keyboard.keycode){
+
+										case ALLEGRO_KEY_ENTER:
+											loginScreen = false;
+											break;
+									}
+								}
+							}
+
+							al_draw_bitmap(BackgroundMenu,0,0,0);	//Printar as informações na tela!
+
+							al_draw_text(fonteHTPTitulo, al_map_rgb(255, 255, 255), LARGURA_TELA / 2, 30, ALLEGRO_ALIGN_CENTRE, "Digite o Login desejado: ");
+
+							if (strlen(loginMsg) > 0){
+
+								al_draw_text(fonteHTP, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+											(ALTURA_TELA - al_get_font_ascent(fonteHTP)) / 2,
+											ALLEGRO_ALIGN_CENTRE, loginMsg);
+							}
+							
+							else{
+
+								al_draw_text(fonteHTP, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+											(ALTURA_TELA - al_get_font_ascent(fonteHTP)) / 2,
+											ALLEGRO_ALIGN_CENTRE, "<login>");
+							}
+
+							//printLoginScreen(loginMsg);
+							al_flip_display();
+							al_clear_to_color(al_map_rgb(0, 0, 0));
+							FPSLimit();
+						}
+
+						connectScreen = 0;
+						inChat = 0;
+					}
+
 					al_start_timer(timer);
-					al_destroy_event_queue(filaEventosMouse);
+					al_destroy_event_queue(filaEventosMouse); 
+					al_destroy_event_queue(filaEventosChat);
 
 					while(inGame){
 
@@ -183,6 +312,7 @@ int main(){
 
 							ALLEGRO_KEY_ESCAPE: 	
 							inGame = 0;
+							apertouBotaoPlay = 0;
 							printf("Apertasse botão ESC\n");
 
 						} else if ( al_key_down(&keyState, ALLEGRO_KEY_W) && delay > 10 ){
@@ -345,3 +475,164 @@ void FPSLimit(){
         al_rest((1.0 / FPS) - getTimer());
     }
 }
+
+//FOR READING KEYBOARD INPUT WITH MAX SIZE = LIMIT AND SAVING AT STR[]
+void readInput(ALLEGRO_EVENT event, char str[], int limit){
+
+    if (event.type == ALLEGRO_EVENT_KEY_CHAR)
+    {
+        if (strlen(str) <= limit)
+        {
+            char temp[] = {event.keyboard.unichar, '\0'};
+            if (event.keyboard.unichar == ' ')
+            {
+                strcat(str, temp);
+            }
+            else if (event.keyboard.unichar >= '!' &&
+                     event.keyboard.unichar <= '?')
+            {
+                strcat(str, temp);
+            }
+            else if (event.keyboard.unichar >= 'A' &&
+                     event.keyboard.unichar <= 'Z')
+            {
+                strcat(str, temp);
+            }
+            else if (event.keyboard.unichar >= 'a' &&
+                     event.keyboard.unichar <= 'z')
+            {
+                strcat(str, temp);
+            }
+        }
+
+        if (event.keyboard.keycode == ALLEGRO_KEY_BACKSPACE && strlen(str) != 0)
+        {
+            str[strlen(str) - 1] = '\0';
+        }
+    }
+}
+
+
+
+
+/* se não pegar Read INPUT
+
+case ALLEGRO_KEY_0:
+
+			if(qtdCharIP < 29){
+				ServerIP[qtdCharIP] = '0';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+
+			}
+			
+		case ALLEGRO_KEY_1:
+
+			if(qtdCharIP < 29){
+				ServerIP[qtdCharIP]= '1';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+				
+			}
+			
+		case ALLEGRO_KEY_2:
+			if(qtdCharIP < 29){
+
+				ServerIP[qtdCharIP]= '2';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+				
+			}
+			
+		case ALLEGRO_KEY_3:
+			if(qtdCharIP < 29){
+
+				ServerIP[qtdCharIP]= '3';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+		
+			}
+			
+		case ALLEGRO_KEY_4:
+			if(qtdCharIP < 29){
+				ServerIP[qtdCharIP]= '4';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+
+			}
+			
+		case ALLEGRO_KEY_5:
+			if(qtdCharIP < 29){
+
+				ServerIP[qtdCharIP]= '5';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+				
+			}
+			
+		case ALLEGRO_KEY_6:
+			if(qtdCharIP < 29){
+
+				ServerIP[qtdCharIP]= '6';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+				
+			}
+			
+		case ALLEGRO_KEY_7:
+			if(qtdCharIP < 29){
+				ServerIP[qtdCharIP]= '7';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+				
+			}
+			
+		case ALLEGRO_KEY_8:
+			if(qtdCharIP < 29){
+
+				ServerIP[qtdCharIP]= '8';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+				
+			}
+			
+		case ALLEGRO_KEY_9:
+			if(qtdCharIP < 29){
+
+				ServerIP[qtdCharIP] = '9';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+			}
+			
+		case ALLEGRO_KEY_FULLSTOP:
+			if(qtdCharIP < 29){
+
+				ServerIP[qtdCharIP] = '.';
+				qtdCharIP++;
+				ServerIP[qtdCharIP]= '\0';
+				break;
+				
+			}
+
+		case ALLEGRO_KEY_BACKSPACE:
+
+			if(qtdCharIP != 0){
+				qtdCharIP--;
+				ServerIP[qtdCharIP]= '\0';
+			}
+
+			break;
+	}
+	}
+
+	*/
